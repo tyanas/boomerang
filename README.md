446a47b445e695b8d16e328bb028f65b90f50602
tyanas <tyanas.go@gmail.com>
2013-08-15 15:49:46 +0400

    minimal express app

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-boomerang
-=========
+#boomerang
+
 
 Summerschool project - Call to the future
+
+##Edit css
+
+Install compass, run `compass watch` in project dir, edit `*.sass` files.
+`compass` will create (or recreate) `*.css` files.
+See `config.rb` for details.
+
+##Deploy tipster
+
+once install heroku toolbelt and (in project root dir):
+
+    git remote add heroku git@heroku.com:.git
+
+when master branch is "stable":
+
+    git co production
+    git merge master
+    git push
+    git push heroku production:master
+
+##Quick install
+
+ Install npm (server side) dependencies:
+
+    $ npm install
+
+ Start the server:
+
+    $ node server
+ 
+ Then open a browser and go to:
+    
+    http://localhost:3000
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-var port = process.env.PORT || 3000;
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(port, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:'+port+'/');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
