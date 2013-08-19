430bea4451ef803d0f70c7174a366d995e596d89
tyanas <tyanas.go@gmail.com>
2013-08-19 19:35:48 +0400

    save numbers

---
+++ app.js
@@ -0,0 +1,53 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , SubscriberProvider = require('./subscriberprovider').SubscriberProvider;
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view options', {layout: false});
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var subscriberProvider= new SubscriberProvider('localhost', 27017);
+
+//Routes
+
+//index
+app.get('/', function(req, res){
+    res.render('index.html', {
+        title: 'Rec.All'
+    });
+});
+
+//save new subscriber
+app.post('/', function(req, res){
+    subscriberProvider.save({
+        phoneNumber: req.param('phoneNumber')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+
+app.listen(process.env.PORT || 3000);
---
+++ subscriberprovider.js
@@ -0,0 +1,98 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+SubscriberProvider = function(host, port) {
+  this.db= new Db('node-mongo-subscriber', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+
+SubscriberProvider.prototype.getCollection= function(callback) {
+  this.db.collection('subscribers', function(error, subscriber_collection) {
+    if( error ) callback(error);
+    else callback(null, subscriber_collection);
+  });
+};
+
+//find all subscribers
+SubscriberProvider.prototype.findAll = function(callback) {
+    this.getCollection(function(error, subscriber_collection) {
+      if( error ) callback(error)
+      else {
+        subscriber_collection.find().toArray(function(error, results) {
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+//find an subscriber by ID
+SubscriberProvider.prototype.findById = function(id, callback) {
+    this.getCollection(function(error, subscriber_collection) {
+      if( error ) callback(error)
+      else {
+        subscriber_collection.findOne({_id: subscriber_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function(error, result) {
+          if( error ) callback(error)
+          else callback(null, result)
+        });
+      }
+    });
+};
+
+
+//save new subscriber
+SubscriberProvider.prototype.save = function(subscribers, callback) {
+    this.getCollection(function(error, subscriber_collection) {
+      if( error ) callback(error)
+      else {
+        if( typeof(subscribers.length)=="undefined")
+          subscribers = [subscribers];
+
+        for( var i =0;i< subscribers.length;i++ ) {
+          subscriber = subscribers[i];
+          subscriber.created_at = new Date();
+        }
+
+        subscriber_collection.insert(subscribers, function() {
+          callback(null, subscribers);
+        });
+      }
+    });
+};
+
+// update an subscriber
+SubscriberProvider.prototype.update = function(subscriberId, subscribers, callback) {
+    this.getCollection(function(error, subscriber_collection) {
+      if( error ) callback(error);
+      else {
+        subscriber_collection.update(
+					{_id: subscriber_collection.db.bson_serializer.ObjectID.createFromHexString(subscriberId)},
+					subscribers,
+					function(error, subscribers) {
+						if(error) callback(error);
+						else callback(null, subscribers)       
+					});
+      }
+    });
+};
+
+//delete subscriber
+SubscriberProvider.prototype.delete = function(subscriberId, callback) {
+	this.getCollection(function(error, subscriber_collection) {
+		if(error) callback(error);
+		else {
+			subscriber_collection.remove(
+				{_id: subscriber_collection.db.bson_serializer.ObjectID.createFromHexString(subscriberId)},
+				function(error, subscriber){
+					if(error) callback(error);
+					else callback(null, subscriber)
+				});
+			}
+	});
+};
+
+exports.SubscriberProvider = SubscriberProvider;
